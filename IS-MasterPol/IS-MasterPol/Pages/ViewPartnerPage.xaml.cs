d9e426edbf9b893ddddf4f06a943a4d02bc19ddf
comcuru <comcuru@DJ-PC-K207-11>
2024-11-18 16:45:06 +0300

    first commit

---
+++ IS-MasterPol/IS-MasterPol/Pages/ViewPartnerPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IS_MasterPol.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ViewPartnerPage.xaml
+    /// </summary>
+    public partial class ViewPartnerPage : Page
+    {
+        public ViewPartnerPage()
+        {
+            InitializeComponent();
+            ViewPartner.ItemsSource = Data.MasterPolDataEntities.GetContext().Partner.ToList();
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void historyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.AddEditPage());
+        }
+    }
+}
